4f58827294f2ba45cb7bdaf393ee7c78f42322f4
zarsko <zarsko2@gmail.com>
2025-12-18 21:24:30 +0200

    Move Firebase config to environment variables for security

--- firebase.ts
+++ firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyACIl-k-LDaMLHkmDxFjigWsDRjWkIROwE",
-  authDomain: "nameit-c440f.firebaseapp.com",
-  projectId: "nameit-c440f",
-  storageBucket: "nameit-c440f.firebasestorage.app",
-  messagingSenderId: "717959116826",
-  appId: "1:717959116826:web:be05dc965592f1866db9a3",
-  measurementId: "G-K7KHWCB1XL"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
